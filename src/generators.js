a401c96b00832fa279db7b927439d65e271a116a
Phillip Wilt <phillip.wilt@gmail.com>
2015-12-01 22:18:17 -0800

    first

---
+++ src/generators.js
@@ -0,0 +1,24 @@
+let evens = {
+	[Symbol.iterator]() {
+		let idx = -2 // assume 0 is the first even
+		return {
+			next() {
+				idx = idx + 2
+				return {
+					done: false,
+					value: idx
+				}
+			}
+		}
+	}
+}
+
+var odds = []
+
+for (let n of evens) {
+	if (n > 100)
+		break
+	odds.push(n + 1)
+}
+
+console.log(odds)
